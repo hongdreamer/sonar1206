71945e4f9045b3c46f56267292ed8f0e469dca11
hong666 <69704308+hongdreamer@users.noreply.github.com>
2022-12-13 07:39:34 +0800

    Update test_views.py

--- Tests/unit_tests/test_views.py
+++ Tests/unit_tests/test_views.py
@@ -37,4 +37,8 @@ class ViewTest(TestCase):
         """Tests the home page."""
         response = self.client.post('/', {'height':1.7,'weight':"60"})
         self.assertContains(response, '20.76', 1, 200, html=True)
+    def test_unit_weight(self):
+        """Tests the home page."""
+        response = self.client.post('/', {'height':1.7,'weight':"300"})
+        self.assertContains(response, '請輸入合理範圍的體重!', 1, 200, html=True)
     
