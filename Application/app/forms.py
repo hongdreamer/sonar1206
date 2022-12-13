683570c64743abcfff1b87272216051a5ca01ec6
hong666 <69704308+hongdreamer@users.noreply.github.com>
2022-12-13 16:23:59 +0800

    Update forms.py

--- Application/app/forms.py
+++ Application/app/forms.py
@@ -27,21 +27,21 @@ class BmiForm(forms.Form):
         widget=forms.TextInput(attrs={'class':'form-control'}),
         error_messages={'required': '請輸入體重'})
     
-def clean(self):
-        cd = self.cleaned_data
+    def clean(self):
+            cd = self.cleaned_data
 
-        try:
-            weight = float(cd.get('weight'))
-            height = float(cd.get('height'))
+            try:
+                weight = float(cd.get('weight'))
+                height = float(cd.get('height'))
 
-            height_condition = (height > 2.5) or (height <= 0)
-            if height_condition:
-                self.add_error('height', "請輸入合理範圍的身高!")
+                height_condition = (height > 2.5) or (height <= 0)
+                if height_condition:
+                    self.add_error('height', "請輸入合理範圍的身高!")
 
-            weight_condition = (weight > 200) or (weight <= 15)
-            if weight_condition:
-                self.add_error('weight', "請輸入合理範圍的體重!")
-        except:
-            self.add_error('weight', "請輸入數字!")
+                weight_condition = (weight > 200) or (weight <= 15)
+                if weight_condition:
+                    self.add_error('weight', "請輸入合理範圍的體重!")
+            except:
+                self.add_error('weight', "請輸入數字!")
 
-        return cd
+            return cd
